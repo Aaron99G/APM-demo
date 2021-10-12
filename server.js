b860bf8bfa17b8c5641d2434ad1ff0082a2e5e09
Aaron Gonzalez <aaronraulgonzalez@gmail.com>
2021-10-12 11:01:53 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+const port = process.env.port || 4400;
+
+app.listen(4400, () => {
+    console.log(`Server is servin at ${port}`)
+})
