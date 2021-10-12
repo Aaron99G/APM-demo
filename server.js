039a5ed112d3853893d2d2b2b1f57d9c19ce9f35
Aaron Gonzalez <aaronraulgonzalez@gmail.com>
2021-10-12 11:27:25 -0500

    Rollbar

--- server.js
+++ server.js
@@ -11,8 +11,21 @@ const rollbar = new Rollbar({
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
     rollbar.info('html file served successfully')
+})
+
+let students = []
+
+app.post(('/api/student'), (req, res) => {
+    let { name } = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log('Student added succesfully', { author: "Aaron", type: 'Manual entry' })
+
+    res.status(200).send(students)
 })
 
 const port = process.env.PORT || 4400;
